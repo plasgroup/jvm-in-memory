def2fabaa08c57fbfac151c23da1e07a00224898
huang <405544641@qq.com>
2023-07-19 18:14:34 +0900

    clean code

--- jdpulib/src/Main.java
+++ jdpulib/src/Main.java
@@ -16,122 +16,19 @@ import static pim.algorithm.TreeWriter.verifyLargePIMTree;
 
 
 public class Main {
-    static void testBST(){
-        ArrayList<BSTBuilder.Pair<Integer, Integer>> pairs =
-                new IntIntValuePairGenerator(100000).genPairs(20000);
-
-        Logger bstTestLogger = Logger.getLogger("bst:testing");
-        TreeNode root = BSTBuilder.buildPIMTree(pairs);
-        int correct = 0;
-
-        for(int i = 0; i < pairs.size(); i++){
-            int key = pairs.get(i).getKey();
-            int val = pairs.get(i).getVal();
-            int retrievedValue = root.search(key);
-            Logger.logf("bst:testing", "==> (Test) search key = %d. correct value = %d, retrieved value = %d\n", key, val, retrievedValue);
-
-            if(val != retrievedValue){
-                bstTestLogger.log("test case fail at index " + i + "\n");
-            }else{
-                correct++;
-            }
-        }
-        bstTestLogger.logf(" == Test finished. Correct %d/%d == \n", correct, pairs.size());
-    }
-
-    static void testBSTCPU(){
-        ArrayList<BSTBuilder.Pair<Integer, Integer>> pairs =
-                new IntIntValuePairGenerator(100000).genPairs(20000);
-
-        Logger bstTestLogger = Logger.getLogger("bst:testing");
-        TreeNode root = BSTBuilder.buildCPUTree(pairs);
-        int correct = 0;
-
-        for(int i = 0; i < pairs.size(); i++){
-            int key = pairs.get(i).getKey();
-            int val = pairs.get(i).getVal();
-            int retrievedValue = root.search(key);
-            Logger.logf("bst:testing", "==> (Test) search key = %d. correct value = %d, retrieved value = %d\n", key, val, retrievedValue);
-
-            if(val != retrievedValue){
-                bstTestLogger.log("test case fail at index " + i + "\n");
-            }else{
-                correct++;
-            }
-        }
-        bstTestLogger.logf(" == Test finished. Correct %d/%d == \n", correct, pairs.size());
-    }
-
-    static int evaluateCPU(){
-        ArrayList<BSTBuilder.Pair<Integer, Integer>> pairs =
-                new IntIntValuePairGenerator(100000000).genPairs(2000000);
-        TreeNode root = BSTBuilder.buildCPUTree(pairs);
-        int i = 0;
-        int s = 0;
-        while(i < 10000000){
-            int query = random.nextInt(2000000);
-            int qk = pairs.get(query).getKey();
-            int v = root.search(qk);
-            s += v;
-            i++;
-        }
-        return s;
-    }
-    static int evaluateDPU(){
-        ArrayList<BSTBuilder.Pair<Integer, Integer>> pairs =
-                new IntIntValuePairGenerator(100000).genPairs(20000);
-        TreeNode root = BSTBuilder.buildPIMTree(pairs);
-
-        int i = 0;
-        int s = 0;
-        while(i < 1000){
-            int query = random.nextInt(20000);
-            int qk = pairs.get(query).getKey();
-            int v = root.search(qk);
-            s += v;
-            i++;
-        }
-        return 0;
-    }
 
 
 
 
-
-
-
-    static Random random = new Random();
-
-        public static void main(String[] args) {
+    public static void main(String[] args) {
         UPMEM.initialize(new UPMEMConfigurator()
                 .setDpuInUseCount(UPMEM.TOTAL_DPU_COUNT)
                 .setThreadPerDPU(UPMEM.perDPUThreadsInUse));
 
         Logger.disableAllBeginWith("pim");
-
-
-            ArrayList<BSTBuilder.Pair<Integer, Integer>> pairs = new IntIntValuePairGenerator(100000)
-                    .genPairs(2000);
-            TreeNode root = buildLargePIMTree(pairs);
-            int correct = 0;
-
-            for(int i = 0; i < pairs.size(); i++){
-                int key = pairs.get(i).getKey();
-                int val = pairs.get(i).getVal();
-                int retrievedValue = root.search(key);
-                Logger.logf("bst:testing", "==> (Test) search key = %d. correct value = %d, retrieved value = %d\n", key, val, retrievedValue);
-
-                if(val != retrievedValue){
-                    Logger.logf("bst:testing" ,"test case fail at index " + i + "\n");
-                }else{
-                    correct++;
-                }
-            }
-
-            System.out.printf(" == Test finished. Correct %d/%d == \n", correct, pairs.size());
+        BSTTester.evaluateCPU(2000000, 1000);
+        //BSTTester.testLargeBST();
     }
-    /*
-    * java -XX:+PreserveFramePointer -Djava.library.path=/home/huang/Desktop/upmem-2023.1.0-Linux-x86_64/lib -Dfile.encoding=UTF-8 -classpath "/media/huang/Local Disk2/jvm-in-memory-dev/jvm-in-memory/jdpulib/out/production/jdpulib":"/media/huang/Local Disk2/jvm-in-memory-dev/jvm-in-memory/lib/dpu.jar" Main
-    * */
+
 
 }
--- jdpulib/src/pim/algorithm/BSTTester.java
+++ jdpulib/src/pim/algorithm/BSTTester.java
@@ -1,4 +1,112 @@
 package pim.algorithm;
 
+import pim.logger.Logger;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import static pim.algorithm.BSTBuilder.buildLargePIMTree;
+
 public class BSTTester {
+    static Random random = new Random();
+    public static void testBST(){
+        ArrayList<BSTBuilder.Pair<Integer, Integer>> pairs =
+                new IntIntValuePairGenerator(100000).genPairs(20000);
+
+        Logger bstTestLogger = Logger.getLogger("bst:testing");
+        TreeNode root = BSTBuilder.buildPIMTree(pairs);
+        int correct = 0;
+
+        for(int i = 0; i < pairs.size(); i++){
+            int key = pairs.get(i).getKey();
+            int val = pairs.get(i).getVal();
+            int retrievedValue = root.search(key);
+            Logger.logf("bst:testing", "==> (Test) search key = %d. correct value = %d, retrieved value = %d\n", key, val, retrievedValue);
+
+            if(val != retrievedValue){
+                bstTestLogger.log("test case fail at index " + i + "\n");
+            }else{
+                correct++;
+            }
+        }
+        bstTestLogger.logf(" == Test finished. Correct %d/%d == \n", correct, pairs.size());
+    }
+
+    public static void testBSTCPU(int totalNodeCount){
+        ArrayList<BSTBuilder.Pair<Integer, Integer>> pairs =
+                new IntIntValuePairGenerator(Integer.MAX_VALUE).genPairs(totalNodeCount);
+
+        Logger bstTestLogger = Logger.getLogger("bst:testing");
+        TreeNode root = BSTBuilder.buildCPUTree(pairs);
+        int correct = 0;
+
+        for(int i = 0; i < pairs.size(); i++){
+            int key = pairs.get(i).getKey();
+            int val = pairs.get(i).getVal();
+            int retrievedValue = root.search(key);
+            Logger.logf("bst:testing", "==> (Test) search key = %d. correct value = %d, retrieved value = %d\n", key, val, retrievedValue);
+            if(val != retrievedValue){
+                bstTestLogger.log("test case fail at index " + i + "\n");
+            }else{
+                correct++;
+            }
+        }
+        bstTestLogger.logf(" == Test finished. Correct %d/%d == \n", correct, pairs.size());
+    }
+
+    public static int evaluateCPU(int totalNodeCount, int queriesCount){
+        ArrayList<BSTBuilder.Pair<Integer, Integer>> pairs =
+                new IntIntValuePairGenerator(Integer.MAX_VALUE).genPairs(totalNodeCount);
+        TreeNode root = BSTBuilder.buildCPUTree(pairs);
+        int i = 0;
+        int s = 0;
+        while(i < queriesCount){
+            int query = random.nextInt(totalNodeCount);
+            int qk = pairs.get(query).getKey();
+            int v = root.search(qk);
+            s += v;
+            i++;
+        }
+        return s;
+    }
+    public static void testLargeBST(){
+        ArrayList<BSTBuilder.Pair<Integer, Integer>> pairs = new IntIntValuePairGenerator(100000)
+                .genPairs(2000);
+        TreeNode root = buildLargePIMTree(pairs);
+        int correct = 0;
+
+        for(int i = 0; i < pairs.size(); i++){
+            int key = pairs.get(i).getKey();
+            int val = pairs.get(i).getVal();
+            int retrievedValue = root.search(key);
+            Logger.logf("bst:testing", "==> (Test#"+ i + ") search key = %d. correct value = %d, retrieved value = %d\n", key, val, retrievedValue);
+
+            if(val != retrievedValue){
+                Logger.logf("bst:testing" ,"test case fail at index " + i + "\n");
+            }else{
+                correct++;
+            }
+        }
+
+        System.out.printf(" == Test finished. Correct %d/%d == \n", correct, pairs.size());
+    }
+    public static int evaluateDPU(){
+        ArrayList<BSTBuilder.Pair<Integer, Integer>> pairs =
+                new IntIntValuePairGenerator(100000).genPairs(20000);
+        TreeNode root = BSTBuilder.buildPIMTree(pairs);
+
+        int i = 0;
+        int s = 0;
+        while(i < 1000){
+            int query = random.nextInt(20000);
+            int qk = pairs.get(query).getKey();
+            int v = root.search(qk);
+            s += v;
+            i++;
+        }
+        return 0;
+    }
+
+
+
 }
