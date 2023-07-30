9f6ae9eef278f4c61f20d17b0e74259af2beaa23
Huang Wanhong <hwanhong@upmemcloud6.cloud.upmem.com>
2023-07-30 06:15:00 +0200

    add records

--- jdpulib/evaluation/profiling-cpu.sh
+++ jdpulib/evaluation/profiling-cpu.sh
@@ -2,7 +2,7 @@
 export SCRIPT_DIR="$(pwd)"
 for i in 1000 2000 5000 10000 20000 50000 100000 200000 500000 1000000 2000000 5000000 10000000
 do
-	perf record -F 20500 -B -e cache-misses,bus-cycles,LLC-loads,LLC-load-misses,LLC-store-misses,LLC-stores,mem_inst_retired.all_loads,mem_inst_retired.all_stores,mem-stores,mem-loads,mem_load_retired.l3_hit,mem_load_retired.l3_miss ~/jdk-17.0.1/bin/java -jar -XX:+UnlockDiagnosticVMOptions -XX:+PreserveFramePointer -XX:+DumpPerfMapAtExit jdpulib.jar CPU 2000000 $i;
+	perf record -F 20500 -B -e LLC-load-misses,LLC-loads,mem-loads,mem-stores,L1-dcache-stores,LLC-store-misses,offcore_response.pf_l3_data_rd.any_response,mem_load_l3_miss_retired.local_dram,mem_load_l3_miss_retired.remote_dram,offcore_requests.all_data_rd,offcore_response.pf_l3_rfo.any_response,offcore_requests.l3_miss_demand_data_rd,offcore_response.all_data_rd.l3_miss.any_snoop,offcore_response.pf_l3_rfo.l3_miss.snoop_miss_or_no_fwd,offcore_response.pf_l3_data_rd.l3_miss.snoop_miss_or_no_fwd,offcore_response.demand_rfo.l3_miss.any_snoop,offcore_response.demand_data_rd.l3_miss_local_dram.snoop_miss_or_no_fwd ~/jdk-17.0.1/bin/java -jar -XX:+UnlockDiagnosticVMOptions -XX:+PreserveFramePointer -XX:+DumpPerfMapAtExit jdpulib.jar CPU 2000000 $i;
 	perf report -f --stdio > "./cpu-only-profile-2000000n-${i}q.txt";
 	perf script --itrace | grep 'search' >  "cpu-only-search-samples-${i}q.txt"; 
 done
