295340e54926bb81d5e9c753d1823e291d042599
huang <405544641@qq.com>
2023-12-12 00:33:28 +0900

    tidy up source code

--- dpu_jvm/dpuslave.c
+++ dpu_jvm/dpuslave.c
@@ -4,9 +4,8 @@
 #include <stdint.h>
 #include <stddef.h>
 #ifndef INMEMORY
-#include <malloc.h>
+#include <malloc.h>  // This part is previously exist because 
 #else
-
 #include <defs.h>
 #include <alloc.h>
 #include <mram.h>
@@ -21,10 +20,6 @@
 #include "core/vm_loop.h"
 #include "utils/jstruct_printer.h"
 #include "sample_ils/test_cases_gen.h"
-
-
-
-__host uint32_t checksum;
 
 char inited = 0;
    
@@ -76,26 +71,25 @@ void print_method(struct j_method __mram_ptr* jm){
 }
 
 void exec_task_from_host() {
-  //  if(me() > 1) return;
     struct function_thunk fc;
     int i;
-    uint8_t __mram_ptr* cpt = m_heapspace;
     int num = 0;
     int t  = 0;
 
-    int tasklet_id = me();
+    int tasklet_id = me(); // get the id of current tasklet.
     struct j_method __mram_ptr *jm = exec_method_pt[tasklet_id];
     struct j_class __mram_ptr *jc = exec_class_pt[tasklet_id];
     int this_tasklet_params_buffer_len = (PARAMS_BUFFER_SIZE / 24);
     int buffer_begin = params_buffer + tasklet_id * this_tasklet_params_buffer_len;
 
+
+
+    // condition judgement to prevent 重复replicative initialization of DPU memory.
     if (inited == 0) {
         init_memory();
         inited = 1;
     }
-   
-    //printf("me = %d, buffer_begin = %p, buffer_pt = %p\n", me(), buffer_begin, params_buffer_pt[tasklet_id]);
-    
+
     int tasklet_buffer_pt = params_buffer_pt[tasklet_id];
 
     if(buffer_begin >= tasklet_buffer_pt){
@@ -115,7 +109,7 @@ void exec_task_from_host() {
                     params_buffer_pt[me()] += 4;    
    
     while(buffer_begin < tasklet_buffer_pt){
-        //DEBUG_PRINT("tasklet_buffer_begin = 0x%x, me = %d\n", buffer_begin, tasklet_id);
+        // DEBUG_PRINT("tasklet_buffer_begin = 0x%x, me = %d\n", buffer_begin, tasklet_id);
 
         /* get a task from parameter buffer */
         int task_id = *(uint32_t __mram_ptr*)buffer_begin;
@@ -126,13 +120,7 @@ void exec_task_from_host() {
         buffer_begin += 4 + fc.func->params_count * 4;
         fc.params = buffer_begin;
 
-        //printf("me = %d, task id = %d, func = %p, jc = %p, current_params = %p, top = %p\n", me(), task_id, fc.func, fc.jc, buffer_begin, tasklet_buffer_pt);
-        //printf("instance = %d, key = %d\n", *(uint8_t __mram_ptr* __mram_ptr*)(buffer_begin - 8), *(uint8_t __mram_ptr* __mram_ptr*)(buffer_begin - 4));
         mem.meta_space = buffer_begin;
-        
-        // print_class(fc.jc);
-        // print_method(fc.func);
-        // print_virtual_table(fc.jc);
         
         current_fp[tasklet_id] = 0;
         current_sp[tasklet_id] = wram_data_space +  tasklet_id * (WRAM_DATA_SPACE_SIZE / 24);
