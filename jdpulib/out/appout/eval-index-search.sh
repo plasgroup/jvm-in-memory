85793d49ea56fc9b03e5d6ca14143b2320741119
huang wanhong <405544641@qq.com>
2024-01-11 22:59:58 +0900

    update scripts

--- jdpulib/out/appout/eval-index-search.sh
+++ jdpulib/out/appout/eval-index-search.sh
@@ -1,1 +1,16 @@
+JAVA=~/jdk-17/bin/java
+DOC_COUNTS=[10000 20000 50000 100000 200000 500000 1000000]
+REQ_COUNT=[200000 500000 1000000 2000000 5000000 10000000]
+THREADS=[1 2 4 8 16 24]
+DPUS=[4 8 16 32 64]
+for cnt_nodes in $NODES_COUNTS do;
+        for cnt_reqs in $REQ_COUNT do;
+                for cnt_threads in $THREADS do;
+                        for cnt_dpus in $DPUS do;
+                                ...
+                        done
+                done
+        done
+done
+$JAVA -cp pimtree.jar:dpu.jar application.transplant.pimtree.PIMTreeMain
 java -cp index-search.jar:dpu.jar application.transplant.index.search.IndexSearchMain
