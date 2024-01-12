ba900196103fb8730adf4d8b1dda5928503afa6c
huang wanhong <405544641@qq.com>
2024-01-12 18:42:18 +0900

    update start-server-script

--- jdpulib/out/appout/start_simulator_server.sh
+++ jdpulib/out/appout/start_simulator_server.sh
@@ -1,4 +1,4 @@
-DPUCOUNT=8
-THREADS_PER_DPU=24
+DPUCOUNT=${1:-8}
+THREADS_PER_DPU=${2:-24}
 echo "start simulator servers...."
-~/jdk-17/bin/java -jar simulator-server.jar DPU_COUNT=$DPUCOUNT THREAD_PER_DPU=$THREADS_PER_DPU
+/home/huang/jdk-17/bin/java -jar simulator-server.jar DPU_COUNT=$DPUCOUNT THREAD_PER_DPU=$THREADS_PER_DPU
