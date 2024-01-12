c5fcd40da659a8752ffb6cff0711edac5de42e98
huang wanhong <405544641@qq.com>
2024-01-12 19:17:16 +0900

    update scripts

--- jdpulib/out/appout/eval-pimtree.sh
+++ jdpulib/out/appout/eval-pimtree.sh
@@ -6,20 +6,22 @@ declare -a NODES_COUNTS=(500 500000 1000000 2000000 5000000 10000000 20000000 50
 declare -a REQ_COUNT=(200 200000 500000 1000000 2000000 5000000 10000000)
 declare -a THREADS=(1 2 4 8 16 24)
 declare -a DPUS=(4 8 16 32 64)
-
+declare -a BATCH=(1 8 16 64 128 200)
 mkdir record-pimtree
 # PIM Version
 for cnt_nodes in ${NODES_COUNTS[@]}; do
 	for cnt_reqs in ${REQ_COUNT[@]}; do
 		for cnt_threads in ${THREADS[@]}; do
 			for cnt_dpus in ${DPUS[@]}; do
-				bash ./kill_all_defunct.sh
-				echo "NODES=$cnt_nodes, REQUEST=$cnt_reqs, THREADS=$cnt_threads, DPUS=$cnt_dpus"
-			        sh start_simulator_server.sh $cnt_dpus $cnt_threads &
-				perf record -e $EVENT_LIST $JAVA $VM_OPTIONS -cp pimtree.jar:dpu.jar application.transplant.pimtree.PIMTreeMain KEYS_COUNT=$cnt_nodes TSK_N=$cnt_reqs
-				perf report --stdio > ./record-pimtree/n${cnt_nodes}-r${cnt_reqs}-t${cnt_threads}-d${cnt_dpus}.txt
-			        kill %1 2> /dev/null && wait $1 2> dev/null
-				bash ./kill_all_defunct.sh
+				for cnt_batch_size in ${BATCH[@]}; do
+				  bash ./kill_all_defunct.sh
+				  echo "NODES=$cnt_nodes, REQUEST=$cnt_reqs, THREADS=$cnt_threads, DPUS=$cnt_dpus"
+			          sh start_simulator_server.sh $cnt_dpus $cnt_threads &
+				  perf record -e $EVENT_LIST $JAVA $VM_OPTIONS -cp pimtree.jar:dpu.jar application.transplant.pimtree.PIMTreeMain KEYS_COUNT=$cnt_nodes TSK_N=$cnt_reqs DPU_COUNT=$cnt_dpus THREADS=$cnt_threads LOAD_BATCH=$cnt_batch_size EXEC_BATCH=$cnt_batch_size PROF_CPUDPU_DM
+				  perf report --stdio > ./record-pimtree/n${cnt_nodes}-r${cnt_reqs}-t${cnt_threads}-d${cnt_dpus}.txt
+			          kill %1 2> /dev/null && wait $1 2> dev/null
+				  bash ./kill_all_defunct.sh
+				done
 			done
 		done
 	done
