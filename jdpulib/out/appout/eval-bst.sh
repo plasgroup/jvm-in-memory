32e5cff50e757b9df0bd6a40e13023fbee39f038
huang wanhong <405544641@qq.com>
2024-01-17 19:42:40 +0900

    update scripts

--- jdpulib/out/appout/eval-bst.sh
+++ jdpulib/out/appout/eval-bst.sh
@@ -5,15 +5,15 @@ VM_OPTIONS="-XX:+UnlockDiagnosticVMOptions -XX:+PreserveFramePointer -XX:+DumpPe
 DICT_PATH=$(pwd)/dict.txt
 FILES_PATH=$(pwd)/files
 REQ_BASE_PATH=$(pwd)
-declare -a NODES_COUNTS=(200000 500000 1000000 2000000 5000000 10000000 20000000 50000000 100000000)
-declare -a REQ_COUNT=(10000 50000 100000 200000)
+declare -a NODES_COUNTS=(500000 1000000 2000000 5000000 10000000 20000000 50000000 100000000)
+declare -a REQ_COUNT=(200000)
 declare -a THREADS=(1 4 8 16 24)
 declare -a DPUS=(64)
 declare -a LAYERS=(18)
 
 
 # PIM Version
-  for cnt_reqs in ${REQ_COUNT[@]}; do
+for cnt_reqs in ${REQ_COUNT[@]}; do
     for cnt_threads in ${THREADS[@]}; do
 	  for cnt_nodes in ${NODES_COUNTS[@]}; do
                         for cnt_layer in ${LAYERS[@]}; do
