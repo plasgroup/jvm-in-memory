71f4cb453822dd7e5857a6b6aad084332e4760e9
Ichinose-Kazuki <ichinose-kazuki657@g.ecc.u-tokyo.ac.jp>
2024-10-21 23:30:12 +0900

    improve setup.sh

--- setup.sh
+++ setup.sh
@@ -3,10 +3,14 @@
 JVM_IN_MEMORY_ROOT_DIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 
 # UPMEM SDK
-wget http://sdk-releases.upmem.com/2023.1.0/debian_10/upmem-2023.1.0-Linux-x86_64.tar.gz
-wget http://sdk-releases.upmem.com/2023.1.0/upmem-src-2023.1.0-Linux-x86_64.tar.gz
-tar xzvf upmem-2023.1.0-Linux-x86_64.tar.gz
-tar xzvf upmem-src-2023.1.0-Linux-x86_64.tar.gz
+if [ ! -d upmem-2023.1.0-Linux-x86_64 ]; then
+  wget http://sdk-releases.upmem.com/2023.1.0/debian_10/upmem-2023.1.0-Linux-x86_64.tar.gz
+  tar xzvf upmem-2023.1.0-Linux-x86_64.tar.gz
+fi
+if [ ! -f upmem-src-2023.1.0-Linux-x86_64.tar.gz ]; then
+    wget http://sdk-releases.upmem.com/2023.1.0/upmem-src-2023.1.0-Linux-x86_64.tar.gz
+    tar xzvf upmem-src-2023.1.0-Linux-x86_64.tar.gz
+fi
 # src/backends/api/src/api/dpu_jni.c に対する指示をやる（共有ライブラリの入れ替えは libdpu.so と dpujni.so だけでいい）
 rm upmem-2023.1.0-Linux-x86_64/src/backends/api/src/api/dpu_jni.c
 cp reproduce/dpu_jni.c upmem-2023.1.0-Linux-x86_64/src/backends/api/src/api/dpu_jni.c
@@ -23,8 +27,10 @@ ln -s libdpujni.so.0.0 libdpujni.so
 cd $JVM_IN_MEMORY_ROOT_DIR
 
 #Java
-wget https://download.oracle.com/java/17/archive/jdk-17.0.11_linux-x64_bin.tar.gz
-tar xzvf jdk-17.0.11_linux-x64_bin.tar.gz
+if [ ! -d jdk-17.0.11 ]; then
+  wget https://download.oracle.com/java/17/archive/jdk-17.0.11_linux-x64_bin.tar.gz
+  tar xzvf jdk-17.0.11_linux-x64_bin.tar.gz
+fi
 PATH=$(pwd)/jdk-17.0.11/bin:$PATH
 
 source upmem-2023.1.0-Linux-x86_64/upmem_env.sh
