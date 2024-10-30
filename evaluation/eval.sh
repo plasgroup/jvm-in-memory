a85843b74f1981e4b342ec15d15fc1d2a49be820
Ichinose Kazuki <ichinose-kazuki657@g.ecc.u-tokyo.ac.jp>
2024-10-30 21:35:24 +0900

    実験用スクリプトの準備

--- evaluation/eval.sh
+++ evaluation/eval.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-OUTPUT_DIR=$(pwd)/$(date '+%Y%m%d_%H%M%S_nbody')
+OUTPUT_DIR=$(pwd)/$(date '+%Y%m%d_%H%M%S_jvm-in-memory_nbody')
 mkdir "$OUTPUT_DIR"
 cd $(dirname "$(readlink -f "$0")")
 
@@ -11,6 +11,9 @@ JAVA=java # ~/jdk-17.0.1/bin/java
 NBODY_JAR="nbody-latest.jar"
 
 ## execute 
-ITERATION=10
-OUTPUT_FILE="$OUTPUT_DIR/jvm-in-memory-$ITERATION.txt"
-$JAVA $VM_OPTIONS -Djava.library.path=../upmem-2023.1.0-Linux-x86_64/lib -cp $NBODY_JAR:dpu.jar application.nbody.Main $ITERATION > $OUTPUT_FILE
+for ITERATIONS in 1 10 100; do
+    for i in {1..10}; do
+        OUTPUT_FILE="$OUTPUT_DIR/jvm-in-memory-$ITERATIONS-$i.txt"
+        $JAVA $VM_OPTIONS -Djava.library.path=../upmem-2023.1.0-Linux-x86_64/lib -cp $NBODY_JAR:dpu.jar application.nbody.Main $ITERATIONS > $OUTPUT_FILE
+    done
+done
